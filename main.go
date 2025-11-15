ee4adc9d07b4d96709b89d917d0c6864ab80ec07
Nikolay Larsen <nikolay@velkomb.com>
2025-11-15 09:48:25 +0500

    Add function stubs for user input and currency conversion

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func getUserInput() string {
+	var userInput string
+	fmt.Scan(userInput)
+	return userInput
+}
+
+func calculateConversion(sum float64, initialCurrency string, targetCurrency string) {
+}
+
 func main() {
 	const USDtoEUR = 0.82
 	const USDtoRUB = 63.52
